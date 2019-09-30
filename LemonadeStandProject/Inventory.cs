5f007762d78e0158a90d626637e5dc33dc841cee
ryanfelicilda <ryan.g.felicilda@gmil.com>
2019-09-30 17:53:42 -0500

    Added member money, lemon, sugar, and ice member variables. Assigned value of member variables in Inventory constructor.

--- LemonadeStandProject/Inventory.cs
+++ LemonadeStandProject/Inventory.cs
@@ -8,5 +8,17 @@ namespace LemonadeStandProject
 {
     class Inventory
     {
+        public double money;
+        public int lemon;
+        public int sugar;
+        public int ice;
+
+        public Inventory()
+        {
+            money = 50.00;
+            lemon = 0;
+            sugar = 0;
+            ice = 0;
+        }
     }
 }
